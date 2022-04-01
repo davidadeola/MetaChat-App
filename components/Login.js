b354da4481e49439207f2b55e69c8e73ca1470aa
davidadeola_ <98563443+davidadeola@users.noreply.github.com>
2022-04-01 19:12:39 +0100

    Profile pic commit

--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import background from '../public/background.jpg';
-import ProfilePic from '../public/ProfilePic.jpg';
+import ProfilePic from '../public/profilePic.jpeg';
 import { useMoralis } from "react-moralis";
 
 function Login() {
     const { authenticate } = useMoralis();
 
   return (
-    <div className="relative bg-black text-white">
+    <div className="relative bg-black text-">
       <h1>Login screen</h1>
       <div
         className="absolute z-50 flex h-4/6 w-full flex-col items-center 
