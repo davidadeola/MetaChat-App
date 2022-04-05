9cb0953ff86f33ad361765b25cd5c82d326228a8
davidadeola_ <98563443+davidadeola@users.noreply.github.com>
2022-04-05 23:10:11 +0100

    Page commit

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,29 @@
-import { useMoralis } from 'react-moralis'
+import Image from 'next/image';
+import { useMoralis } from 'react-moralis';
+
+import profilePic from '../public/profilePic.jpeg';
+import Avatar from './Avatar'
 
 function Header() {
     const { user } = useMoralis();
 
   return (
     <div>
-    <h1>The Header section</h1>
+      <div className="">
+        <div className="relative mx-auto hidden h-24 w-24 lg:inline-grid">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+            src={profilePic}
+          />
+        </div>
+        <div>
+          <div>
+            <Avatar logoutOnPress />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: [],
+    domains: [ 'avatars.dicebear.com' ],
   },
   reactStrictMode: true,
 }
